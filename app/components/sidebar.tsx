bbbdd21eec3969f20a0d726a8ca92bf8f28a1df1
qianxiaobin <qianxiaobin@heidideMacBook-Air.local>
2023-07-01 11:18:35 +0800

    修改title

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -117,10 +117,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT
+          自动机器人
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          机器人...
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
